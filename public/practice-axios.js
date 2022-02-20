4a86e391536f62722878efec137c0be99b3a37e9
James Gitchell <jamesgitchell.dev@gmail.com>
2022-02-19 20:15:45 -0500

    kinda lost here- why no console.log

--- public/practice-axios.js
+++ public/practice-axios.js
@@ -1,3 +1,27 @@
-const axios = require("axios")
+/*
+const axios = require("axios");
 
-const BASE_URL = "http://localhost:5000/constellations";
+const constellationsUrlL = "http://localhost:5000/constellations";
+/*
+axios
+    .get(BASE_URL)
+      .then(({data})=> {
+          const exists = data.find(({ name }) => name === localStorage.name);
+          if (exists) throw `Constellation "${leo.name}" already exists.`
+          return axios
+                      .post(BASE_URL, leo)
+                        .then(({ data }) => console.log(data));
+                          })
+                          .catch(console.log);*/
+
+/*axios
+  .get(constellationsUrl)
+  .then(({ data }) => {
+    const exists = data.find(({ name }) => name === leo.name);
+    if (exists) throw `Constellation "${leo.name}" already exists.`;
+    return axios
+      .post(constellationsUrl, leo)
+      .then(({ data }) => console.log(data));
+  })
+  .catch(console.log);
+*/
--- requests-in-javascript/requests.js
+++ requests-in-javascript/requests.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+/*const axios = require("axios");
 
 const url = "http://localhost:5000/constellations"
 axios
@@ -10,4 +10,30 @@ axios
      })
      .catch((error) => {
          console.log(error.message);
-     });
+     });*/
+
+     const axios = require("axios");
+
+const constellationsUrlL = "http://localhost:5000/constellations";
+/*
+axios
+    .get(BASE_URL)
+      .then(({data})=> {
+          const exists = data.find(({ name }) => name === localStorage.name);
+          if (exists) throw `Constellation "${leo.name}" already exists.`
+          return axios
+                      .post(BASE_URL, leo)
+                        .then(({ data }) => console.log(data));
+                          })
+                          .catch(console.log);*/
+
+axios
+  .get(constellationsUrl)
+  .then(({ data }) => {
+    const exists = data.find(({ name }) => name === leo.name);
+    if (exists) throw `Constellation "${leo.name}" already exists.`;
+    return axios
+      .post(constellationsUrl, leo)
+      .then(({ data }) => console.log(data));
+  })
+  .catch(console.log);
